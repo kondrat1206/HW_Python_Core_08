65a71e171cf16a4fdfdb25f9bf0767a1c98b69f1
kondrat1206 <kondrat1206@gmail.com>
2023-08-23 11:59:58 +0300

    Fix

--- main.py
+++ main.py
@@ -1,11 +1,36 @@
 from datetime import datetime, timedelta
 
 
-def get_celebrators(users, now):
-    # Определяем, у кого день рождения был вчера и в ближайшие 6 дней
-    celebrators = []
+now = datetime.now().date()
+
+
+def get_celebrate_per_week(users):
+    # Переносим празднование ДР на следующий понедельник, если ДР в субботу, или воскресенье
+    celebrate_users = []
     for user in users:
-        if now - timedelta(days=1) <= user['birthday'].date() <= now + timedelta(days=6):
+        celebrate_user = {}
+        if user['birthday'].weekday() == 6:
+            celebrate_user['name'] = user['name']
+            celebrate_user['birthday'] = user['birthday'] + timedelta(days=1)
+            celebrate_users.append(celebrate_user)
+        elif user['birthday'].weekday() == 5:
+            celebrate_user['name'] = user['name']
+            celebrate_user['birthday'] = user['birthday'] + timedelta(days=2)
+            celebrate_users.append(celebrate_user)
+        else:
+                celebrate_user['name'] = user['name']
+                celebrate_user['birthday'] = user['birthday']
+                celebrate_users.append(celebrate_user)
+
+    return celebrate_users
+
+
+def get_celebrators(users, now=now):
+    # Определяем, у кого празднуем день рождения в ближайшие 7 дней
+    celebrate_users = get_celebrate_per_week(users)
+    celebrators = []
+    for user in celebrate_users:
+        if now  < user['birthday'].date() <= now + timedelta(days=7):
             celebrate_user = {}
             celebrate_user['name'] = user['name']
             celebrate_user['birthday'] = user['birthday']
@@ -14,29 +39,11 @@ def get_celebrators(users, now):
     return celebrators
 
 
-def get_celebrate_per_week(users, now):
-    # Определяем, у кого день рождения в воскресенье и увеличиваем ему дату поздравления на 1 день
-    # остальным убираем проверку на вчера
-    celebrate_users = []
-    for user in users:
-        celebrate_user = {}
-        if user['birthday'].weekday() == 6:
-            celebrate_user['name'] = user['name']
-            celebrate_user['birthday'] = user['birthday'] + timedelta(days=1)
-            celebrate_users.append(celebrate_user)
-        else:
-            if now <= user['birthday'].date() <= now + timedelta(days=6):
-                celebrate_user['name'] = user['name']
-                celebrate_user['birthday'] = user['birthday']
-                celebrate_users.append(celebrate_user)
-
-    return celebrate_users
-
-
-def get_birthdays_per_week(users, now):
+def get_birthdays_per_week(users, now=now):
+    #получаем пользователей, которых надо поздравить в ближайшие 7 дней
+    users = get_celebrators(users)
     # находим первый понедельник на этой неделе
     start_of_week = now - timedelta(days=now.weekday())
-    
     # Создаем словарь для сохранения поздравлений по дням недели
     birthdays_per_week = {
         'Monday': [],
@@ -52,10 +59,8 @@ def get_birthdays_per_week(users, now):
     for user in users:
         name = user['name']
         birthday = user['birthday'].date()
-        
         # Определяем день недели для его поздравления
         day_of_week = (start_of_week + timedelta(days=(birthday.weekday() - start_of_week.weekday()) % 7)).strftime('%A')
-        
         # Добавляем пользователя в словарь по дням недели
         birthdays_per_week[day_of_week].append(name)
     
@@ -65,32 +70,22 @@ def get_birthdays_per_week(users, now):
             print(f"{day}: {', '.join(names)}")
 
 
-def start(test_users):
-    # Определяем текущий день
-    now = datetime.now().date()
-    # Определяем, у кого день рождения в ближайшие 7 дней
-    celebrators = get_celebrators(test_users, now)
-    # Определяем, у кого день рождения в воскресенье и увеличиваем ему дату поздравления на 1 день
-    celebrate_users = get_celebrate_per_week(celebrators, now)
-    # Сортируем пользователей и выводим на печать именинников
-    get_birthdays_per_week(celebrate_users, now)
+users = [
+    {'name': 'Bill', 'birthday': datetime(2023, 8, 19)}, # сб
+    {'name': 'Jill', 'birthday': datetime(2023, 8, 20)}, # вс
+    {'name': 'Kim', 'birthday': datetime(2023, 8, 21)}, # пн
+    {'name': 'Jan', 'birthday': datetime(2023, 8, 22)}, # вт
+    {'name': 'Kate', 'birthday': datetime(2023, 8, 23)}, # ср
+    {'name': 'Ane', 'birthday': datetime(2023, 8, 24)}, # чт
+    {'name': 'Serg', 'birthday': datetime(2023, 8, 25)}, # пт
+    {'name': 'Natali', 'birthday': datetime(2023, 8, 26)}, # сб
+    {'name': 'Petr', 'birthday': datetime(2023, 8, 27)}, # вс
+    {'name': 'Evgen', 'birthday': datetime(2023, 8, 28)}, # пн
+    {'name': 'Mary', 'birthday': datetime(2023, 8, 29)}, # вт
+    {'name': 'Nik', 'birthday': datetime(2023, 8, 30)}, # ср
+    {'name': 'Andry', 'birthday': datetime(2023, 8, 31)}, # чт
 
-
-
-
-
-# Тестовый список пользователей
-test_users = [
-    {'name': 'Bill', 'birthday': datetime(2023, 8, 27)},
-    {'name': 'Jill', 'birthday': datetime(2023, 8, 22)},
-    {'name': 'Kim', 'birthday': datetime(2023, 8, 25)},
-    {'name': 'Jan', 'birthday': datetime(2023, 8, 25)},
-    {'name': 'Kate', 'birthday': datetime(2023, 8, 20)},
 ]
 
 
-
-if __name__ == '__main__':
-
-    start(test_users)
-
+get_birthdays_per_week(users)
